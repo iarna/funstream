3662e1c0304cc370a63ea8cd1657177fe5e497f6
Rebecca Turner <me@re-becca.org>
2017-08-08 14:07:07 -0700

    map-stream: Use true privates

--- map-stream.js
+++ map-stream.js
@@ -4,6 +4,8 @@ const FunTransform = require('./fun-transform.js')
 
 module.exports = MapStream
 
+const MAPS = Symbol('maps')
+
 function MapStream (mapWith, opts) {
   if (FunStream.isAsync(mapWith, 1, opts)) {
     return new MapStreamAsync(mapWith, opts)
@@ -15,14 +17,14 @@ function MapStream (mapWith, opts) {
 class MapStreamAsync extends FunTransform {
   constructor (mapWith, opts) {
     super(opts)
-    this.maps = [mapWith]
+    this[MAPS] = [mapWith]
   }
   _transform (data, encoding, next) {
     this._runMaps(data, 0, next)
   }
   _runMaps (data, nextMap, next) {
     try {
-      if (nextMap >= this.maps.length) {
+      if (nextMap >= this[MAPS].length) {
         this.push(data)
         return next()
       }
@@ -33,7 +35,7 @@ class MapStreamAsync extends FunTransform {
           this._runMaps(value, nextMap + 1, next)
         }
       }
-      const result = this.maps[nextMap](data, handleResult)
+      const result = this[MAPS][nextMap](data, handleResult)
       if (result && result.then) return result.then(keep => handleResult(null, keep), handleResult)
     } catch (ex) {
       next(ex)
@@ -41,7 +43,7 @@ class MapStreamAsync extends FunTransform {
   }
   map (mapWith, opts) {
     if (FunStream.isAsync(mapWith, 1, opts)) {
-      this.maps.push(mapWith)
+      this[MAPS].push(mapWith)
       return this
     } else {
       return super.map(mapWith, opts)
@@ -52,11 +54,11 @@ class MapStreamAsync extends FunTransform {
 class MapStreamSync extends FunTransform {
   constructor (mapWith, opts) {
     super(opts)
-    this.maps = [mapWith]
+    this[MAPS] = [mapWith]
   }
   _transform (data, encoding, next) {
     try {
-      this.push(this.maps.reduce((data, fn) => fn(data), data))
+      this.push(this[MAPS].reduce((data, fn) => fn(data), data))
       next()
     } catch (ex) {
       next(ex)
@@ -66,7 +68,7 @@ class MapStreamSync extends FunTransform {
     if (FunStream.isAsync(mapWith, 1, opts)) {
       return super.map(mapWith, opts)
     } else {
-      this.maps.push(mapWith)
+      this[MAPS].push(mapWith)
       return this
     }
   }
